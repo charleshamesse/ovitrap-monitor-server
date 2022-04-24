bdfb54024f8d3f5d8e4c840898647daa94f1aab1
Charles Hamesse <charles.hamesse+conae@pm.me>
2022-04-24 12:11:12 -0300

    setting signing method for image upload, cont'd

--- oeg/views.py
+++ oeg/views.py
@@ -11,19 +11,15 @@ def sign_s3(request):
     file_name =  request.GET.get('file_name')
     file_type = "image/jpeg" # request.args.get('file_type')
 
-    s3 = boto3.client('s3')
+    s3 = boto3.client('s3', config = Config(signature_version = 's3v4'))
 
     presigned_post = s3.generate_presigned_post(
         Bucket = S3_BUCKET,
         Key = file_name,
-        Fields = {"acl": "public-read", "Content-Type": file_type},
+        Fields = { "acl": "public-read", "Content-Type": file_type},
         Conditions = [
-            {
-                "acl": "public-read"
-            },
-            {
-                "Content-Type": file_type
-            }
+            { "acl": "public-read" },
+            { "Content-Type": file_type }
         ],
         ExpiresIn = 3600
     )
