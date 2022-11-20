90ec5e057ec43cb77c0b3089c1ae491d5154d9a3
Charles Hamesse <charles.hamesse+conae@pm.me>
2022-11-20 13:13:51 +0100

    test to connect with railway db

--- oeg/settings.py
+++ oeg/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'oeg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'none'),
+        'HOST': 'containers-us-west-113.railway.app',
+        'PORT': '6575',
     }
 }
 
